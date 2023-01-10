8db149747d30ecc491b783ec732db8a8cd95309b
lisalindqvist95 <lindqvist.lisa@hotmail.com>
2023-01-10 13:15:52 +0000

    Create function for user to review their drink.

--- review.py
+++ review.py
@@ -7,11 +7,11 @@ def review():
     while True:
         try:
             rating = int(input("På en skala på 1-5 hur mycket skulle du rekommendera den här drycken till maten?")) 
+            if rating > 5 or rating < 1:
+                raise ValueError()
             break
         except ValueError:
-            print("Oops!  That was no valid number.  Try again...")
-        except rating > 5:
-          print("Oops!  That was no valid number.  Try again...")
+            print("That was no valid choice, please choose a number between 1-5.")
 
     print(f"Du har ätit {food} och druckit {drink}, och du skulle ge denna kombinationen en rating på {rating} av 5.")
 
